c5f878bb247471e6083172d8709b820d5204c0e1
mirael-miracle <salyutin1@gmail.com>
2021-02-01 14:39:25 +0300

    lb9 Ionic

---
+++ lab9/first-ion/src/app/mytab/mytab.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mytab',
+  templateUrl: './mytab.page.html',
+  styleUrls: ['./mytab.page.scss'],
+})
+export class MytabPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
